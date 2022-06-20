71d8e4c578b411c3c980c96857da9eb0a12cf9d6
Jeremy <jeremy.pionsfr@gmail.com>
2022-06-20 21:36:40 +0200

    Exercice 6: get pour un dossier

---
+++ app/Http/Controllers/DossierController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class DossierController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index($uuid)
+    {
+        $dossiers = DB::select("select * from dossiers where uuid='" .$uuid."'");
+        return response()->json([
+            'dossiers' => $dossiers
+        ], 200);
+    }
+
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\DossierController;
 use App\Http\Controllers\EcritureController;
 use Illuminate\Support\Facades\Route;
 /*
@@ -18,3 +19,7 @@ Route::post('/comptes/{uuid}/ecritures', [EcritureController::class ,'store']);
 
 Route::put('/comptes/{uuid}/ecritures/{uuid_ecriture}', [EcritureController::class ,'update']);
 Route::delete('/comptes/{uuid}/ecritures/{uuid_ecriture}', [EcritureController::class ,'destroy']);
+
+
+
+Route::get('/dossiers/{uuid}', [DossierController::class ,'index']);
